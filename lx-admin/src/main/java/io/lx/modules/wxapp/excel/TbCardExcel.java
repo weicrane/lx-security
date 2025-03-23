031daaba8d7c1d3c0aee0d0d552e1a29ab99ed54
mofeng <laoniane@gmail.com>
2025-03-23 23:26:10 +0800

    导出功能

--- lx-admin/src/main/java/io/lx/modules/wxapp/excel/TbCardExcel.java
+++ lx-admin/src/main/java/io/lx/modules/wxapp/excel/TbCardExcel.java
@@ -1,6 +1,7 @@
 package io.lx.modules.wxapp.excel;
 
 import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.annotation.write.style.ColumnWidth;
 import lombok.Data;
 
 import java.util.Date;
@@ -13,22 +14,38 @@ import java.util.Date;
  */
 @Data
 public class TbCardExcel {
-    @ExcelProperty(value = "主键ID")
+    @ExcelProperty(value = "卡ID")
+    @ColumnWidth(20) // 设置列宽为 20
     private Integer id;
-    @ExcelProperty(value = "16位卡密")
+
+    @ExcelProperty(value = "兑换码（请注意保密）")
+    @ColumnWidth(30) // 设置列宽为 20
     private String cardCode;
-    @ExcelProperty(value = "状态(0:未使用, 1:已使用)")
+
+    @ExcelProperty(value = "状态(0-未使用，1-已使用)")
+    @ColumnWidth(15) // 设置列宽为 20
     private Integer status;
+
     @ExcelProperty(value = "有效期")
+    @ColumnWidth(30) // 设置列宽为 20
     private Date expireTime;
-    @ExcelProperty(value = "类别，00-终身会员，01-网盘路书，02-自驾活动，03-四季玩法")
+
+    @ExcelProperty(value = "类别(00-会员，03-线路)")
+    @ColumnWidth(15) // 设置列宽为 20
     private String productType;
-    @ExcelProperty(value = "线路玩法ID")
+
+    @ExcelProperty(value = "线路ID")
+    @ColumnWidth(15) // 设置列宽为 20
     private Integer routesGuidesId;
+
     @ExcelProperty(value = "创建时间")
+    @ColumnWidth(30) // 设置列宽为 20
     private Date createdAt;
+
     @ExcelProperty(value = "更新时间")
+    @ColumnWidth(30) // 设置列宽为 20
     private Date updatedAt;
+
     @ExcelProperty(value = "备注")
     private String description;
 
